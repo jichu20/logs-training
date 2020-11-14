3b43e760d80a1c062acb58d53056656cd004992c
Borja Javier Sánchez Yuste <borjajavier.sanchez.next@bbva.com>
2020-11-14 18:05:32 +0100

    change path

--- logger-server/src/main/java/com/jichu20/loggerserver/controller/BookController.java
+++ logger-server/src/main/java/com/jichu20/loggerserver/controller/BookController.java
@@ -8,18 +8,20 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/book")
 public class BookController {
-    
+
     private static final Logger logger = LoggerFactory.getLogger(BookController.class);
 
     @Autowired
     BookService bookService;
 
-    @GetMapping("/")
+    @GetMapping("/{bookName}")
     public ResponseEntity<BookDto> getBook(String bookName) throws InterruptedException {
 
         logger.info("Hello Sleuth - server");
